bcf4e9c80c2bab6392b6547e2fa0e51ecf92714d
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-11-18 21:12:35 +0000

    ade game over screne and increase fps

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -67839,7 +67839,7 @@ var handlePoseToGameEvents = function handlePoseToGameEvents(pose) {
 }; // fps for predictions
 
 
-var fps = 8;
+var fps = 20;
 var then = Date.now();
 var now, delta;
 var interval = 1000 / fps;
@@ -67930,7 +67930,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53338" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62883" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
